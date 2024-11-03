2d3cb567058f138bfcbf612d0d920e7d18035653
ArcaneCipher <desjardins.jarp@gmail.com>
2024-11-03 15:00:04 -0500

    Add second route and template. Add 'shorten a new url' link to urls_show.ejs and urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -16,6 +16,18 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  // const templateVars could be simplified as:
+  // const id = req.params.id; // Retrieve the short URL ID
+  // const longURL = urlDatabase[id]; // Retrieve the long URL from the urlDatabase
+  // const templateVars = { id, longURL }; // Pass both id and longURL to the template
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
